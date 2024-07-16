5d184494c016285c970e476262d717a3be125c94
557 <1848395039@qq.com>
2024-07-17 07:26:52 +0800

    base03

---
+++ 3.Coding/7.HZOJ-838.cpp
@@ -0,0 +1,57 @@
+/*************************************************************************
+	> File Name: HZOJ-838.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 15 Jul 2024 08:04:02 AM CST
+ ************************************************************************/
+/*************************************************************************
+    > File Name: solve.cpp
+    > Author:
+    > Mail:
+    > Created Time:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+using namespace std;
+
+int min_num(int a, int b, int c) {
+    if (a > b) swap(a, b);
+    if (a > c) swap(a, c);
+    return a;
+}
+
+int func(queue<int> que1, queue<int> que2, queue<int> que3) {
+    int ans = 0x3f;
+    while(!que1.empty() && !que2.empty() && !que3.empty())
+    {
+        int a = que1.front(), b = que2.front(), c = que3.front();
+        int tmp_ans = abs(a - b) + abs(a - c) + abs(b - c);
+        if (tmp_ans < ans) ans = tmp_ans;
+        int tmp_num = min_num(a, b, c);
+        if (a == tmp_num) que1.pop();
+        if (b == tmp_num) que2.pop();
+        if (c == tmp_num) que3.pop();
+    }
+    return ans;
+}
+
+int main() {
+    int m, n, k, x;
+    queue<int> que1, que2, que3;
+    cin >> m >> n >> k;
+    for (int i = 0; i < m; i++) {
+        cin >> x;
+        que1.push(x);
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        que2.push(x);
+    }
+    for (int i = 0; i < k; i++) {
+        cin >> x;
+        que3.push(x);
+    }
+    cout << func(que1, que2, que3) << endl;
+    return 0;
+}
